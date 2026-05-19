4cc99435108cca4d48bab960665170ab5e3129ee
rizzwaaaan <rizwan8.2004@gmail.com>
2026-05-19 21:00:16 +0530

    Initialise Springboot

---
+++ src/main/java/com/kakak/kakak_backend/KakakBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kakak.kakak_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakakBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakakBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakak/kakak_backend/KakakBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakak.kakak_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakakBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
