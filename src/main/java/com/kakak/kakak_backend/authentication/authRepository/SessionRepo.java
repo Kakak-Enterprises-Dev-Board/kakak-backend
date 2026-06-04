e61f52dd0748bfe6c22d7a81d9f53d20dfd2c640
rohan-K210 <rohankrishna8a@gmail.com>
2026-06-05 01:27:34 +0530

    fixed bugs,completed unit testing

--- src/main/java/com/kakak/kakak_backend/authentication/authRepository/SessionRepo.java
+++ src/main/java/com/kakak/kakak_backend/authentication/authRepository/SessionRepo.java
@@ -3,12 +3,22 @@ package com.kakak.kakak_backend.authentication.authRepository;
 import com.kakak.kakak_backend.authentication.authEntity.AuthSession;
 import com.kakak.kakak_backend.authentication.authEntity.AuthUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SessionRepo
         extends JpaRepository<AuthSession, UUID> {
+    @Query("""
+SELECT s
+FROM AuthSession s
+WHERE s.refresh_token = :token
+""")
+    Optional<AuthSession> findByRefreshToken(
+            @Param("token") String token);
 
     List<AuthSession> findByUser(AuthUsers user);
 }
