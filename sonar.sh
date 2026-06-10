e1a5131c69b3d7192af74e55ccf14293af0547d4
krishnaprasadsmwork <krishnaprasadsm.work@gmail.com>
2026-06-10 21:58:53 +0530

    sonar code

--- sonar.sh
+++ sonar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mvn clean verify sonar:sonar \
--Dsonar.projectKey=Kakak \
--Dsonar.host.url=http://localhost:9000 \
--Dsonar.token=sqp_bab5d0f5a19cfead6b3036ad987535fdac7d2bd0
+  -Dsonar.projectKey=Kakak-Enterprises-Dev-Board_kakak-backend_aa62c8c0-5169-44a8-80ab-67f6d7dfb943 \
+  -Dsonar.projectName=kakak-backend \
+  -Dsonar.host.url=https://sonar.orque-ims.xyz \
+  -Dsonar.token=sqp_e97ff7d7779394d0136737eba1986746db436616
